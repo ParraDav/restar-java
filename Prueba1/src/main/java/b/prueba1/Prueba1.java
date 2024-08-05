d43e679a8fdafadd7074af3d21da17961735bcbd
samuel <mafevarey@hotmail.com>
2024-08-05 08:41:15 -0500

    1.0

---
+++ Prueba1/src/main/java/b/prueba1/Prueba1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package b.prueba1;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Prueba1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
